3a5fee4fd208f8b0b5beba4cb1d89044eeb085db
EnesGerem <enesgerem@gmail.com>
2020-12-05 17:27:11 +0300

    finished

--- AES.java
+++ AES.java
@@ -16,13 +16,15 @@ public class AES {
         secretKey = new SecretKeySpec(key, "AES");
     }
 
-    public byte[] encrypt(byte[] plaintext) throws BadPaddingException, IllegalBlockSizeException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
+    public byte[] encrypt(byte[] plaintext) 
+            throws BadPaddingException, IllegalBlockSizeException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
         Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
         cipher.init(Cipher.ENCRYPT_MODE, secretKey);
         return Base64.getEncoder().encode(cipher.doFinal(plaintext));
     }
 
-    public byte[] decrypt(byte[] ciphertext) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+    public byte[] decrypt(byte[] ciphertext) 
+            throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
         Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
         cipher.init(Cipher.DECRYPT_MODE, secretKey);
         return cipher.doFinal(Base64.getDecoder().decode(ciphertext));
